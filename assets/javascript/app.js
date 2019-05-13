809e1ed7ff07154a8cdb5dc5a632e99390c4010e
C Howe <brook.theis@gmail.com>
2019-05-13 11:04:30 -0600

    fixed countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -87,7 +87,7 @@ minute = second * 60,
 hour = minute * 60,
 day = hour * 24;
 
-let countDown = new Date('Dec 25, 2018 00:00:00').getTime(),
+let countDown = new Date('Dec 25, 2019 00:00:00').getTime(),
 x = setInterval(function() {
 
     let now = new Date().getTime(),
